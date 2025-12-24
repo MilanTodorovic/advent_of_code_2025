331a8ed1fd939b4c6824f36cd50e95fd61402959
Milan Todorovic <todor-90@hotmail.com>
2025-12-24 15:59:57 +0100

    Update Day_3.py
    Still not done. Recursion is hard for my smooth brain :(

--- Day_3.py
+++ Day_3.py
@@ -1,6 +1,5 @@
-
-
 DATA: list[str] = []
+DIGITS = 12
 
 def parse_file():
 	with open("input_3.txt", "r") as f:
@@ -22,10 +21,43 @@ def part_one():
 		result += int(res)
 	print(f"Part one {result=}")
 
+def find_max(s: str, p_idx: int):
+	global DIGITS
+
+	print(f"\tSubstring {s:1} and {p_idx=}.")
+
+	if len(s)-1 <= 0:
+		return []
+	else:
+		big = max(s)
+		_idx = s.index(big)
+		DIGITS -= 1
+		
+		if DIGITS == 0:
+			return [p_idx+_idx]
+
+		tail = len(s[_idx+1:])
+		if tail == DIGITS:
+			# return indecis for all the numbers to the right including the big
+			# if there are no numbers on the right, returns just the big -> range(6, 6+0+1) -> [6]
+			return [p_idx+i for i in range(_idx, _idx+tail+1)]
+		elif tail > DIGITS:
+			return [p_idx+_idx] + find_max(s[_idx+1:], p_idx+_idx)
+		else:
+			# not enough digits on the right side, must supplement from the left
+			return [p_idx+_idx] + find_max(s[p_idx+_idx+1:], p_idx+_idx) + find_max(s[:_idx], p_idx)
+
 def part_two():
+	global DIGITS
+
 	result: int = 0
 	for row in DATA:
-		nums: list[str] = list(row) # get individual numebrs as chars
+		DIGITS = 12
+		print("Starting number:", row)
+		res: list[int] = find_max(row, 0)
+		res.sort()
+		print(f"Final {res=} |", row, " -> ", "".join(row[i] for i in res), "\n")
+		
 
 	print(f"Part two {result=}")
 
@@ -33,4 +65,5 @@ def part_two():
 if __name__ == "__main__":
 	parse_file()
 	part_one() # 17074
+
 	part_two() # ???
