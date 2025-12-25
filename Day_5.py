c44852d0f66184aa4d3813a78e87ad61a1744c71
Milan Todorovic <todor-90@hotmail.com>
2025-12-25 19:36:48 +0100

    Update Day_5.py
    Turns out I misunderstood the assignment for the second part :-D

--- Day_5.py
+++ Day_5.py
@@ -6,7 +6,7 @@ def parse_file():
 	ingredients = []
 
 	store_in_ranges = True
-	with open("input_5_test.txt", 'r') as f:
+	with open("input.txt", 'r') as f:
 		for line in f.readlines():
 			if line == "\n":
 				store_in_ranges = False
@@ -21,14 +21,11 @@ def parse_file():
 	return ranges, ingredients
 
 
-def merge_ranges(ranges):
-
-	merged = True
+def merge_ranges(ranges: deque[list[int]]):
 	NEW_RANGES = deque()
 	to_remove = []
-	while merged or len(ranges):
-		print(ranges)
-		# TODO zajebava u test fajlu
+
+	while len(ranges) > 1:
 		a = ranges.popleft()
 		for b in ranges:
 			# print(f"Checking range {b=} against range {a=}.")
@@ -36,72 +33,60 @@ def merge_ranges(ranges):
 				# print(f"Found overlap - {a=} merged with {b=}.")
 				a[0], a[1] = a[0], a[1] if a[1] > b[1] else b[1]
 				to_remove.append(b)
-				merged = True
 			elif a[0] >= b[0] and a[0] <= b[1]:
 				# print(f"Found overlap - {a=} merged with {b=}.")
 				a[0], a[1] = b[0], a[1] if a[1] > b[1] else b[1]
 				to_remove.append(b)
-				merged = True
 			elif a[1]+1 == b[0]:
 				# No gap, but also no overlap 12-20 and 21-30. The ranges are inclusive
 				# 	so we merge them together
 				# print(f"Found overlap - {a=} merged with {b=}.")
 				a[0], a[1] = a[0], b[1]
 				to_remove.append(b)
-				merged = True
 			else:
-				# if no merges happened at all, this variables stays False
-				merged = False
+				# no merge can happen
+				pass
 		else:
 			NEW_RANGES.append(a)
 			while len(to_remove):
 				ranges.remove(to_remove.pop())
-			# TODO ovo nece
-			if not len(ranges):
-				break
-
+	if len(ranges):
+		NEW_RANGES.append(ranges.pop())
 	ranges = NEW_RANGES
 	# print(f"Merged ranges: ", sorted(ranges))
 	return ranges
 
 
-def part_one(ranges, ingredients):
+def part_one(ranges: deque[list[int]], ingredients: list[int]):
 	fresh = 0
-	local_ingredients = ingredients[::]
+	_ingredients = ingredients[:]
 	for _range in ranges:
+		to_remove = set()
 		left, right = _range
-		j = 0
-		for i, ingredient in zip(range(len(ingredients)), ingredients):
+		i = 0
+		for ingredient in _ingredients:
 			# print(f"Range {left}-{right} and ingredient {ingredient}.")
 			if ingredient >= left and ingredient <= right:
 				fresh += 1
-				local_ingredients.pop(i-j)
-				j += 1
+				to_remove.add(i)
 				# print(f"Ingredient {ingredient} is fresh and in range {left}-{right}.")
+			i += 1
+		else:
+			to_remove = sorted(to_remove)
+			while len(to_remove):
+				_ingredients.pop(to_remove.pop())
 	print(f"Part One: {fresh}")
 
 
-def part_two(ranges, ingredients):
-	print(ranges, ingredients)
+def part_two(ranges: deque[list[int]]):
 	fresh = 0
-	s = []
-	for _range in ranges:
-		left, right = _range
-		local_ingredients = ingredients[::]
-		j = 0
-		for i, ingredient in zip(range(len(local_ingredients)), local_ingredients):
-			if ingredient >= left and ingredient <= right:
-				# print(f"Popping elemnet {i}")
-				ingredients.pop(i-j)
-				s.append(_range)
-				j += 1
-	for r in s:
-		fresh += r[1]-r[0]+1
+	for r in ranges:
+		fresh += r[1]-r[0]+1 # adding +1 because both sides of the range are inclusive
 	print(f"Part Two: {fresh}")
 
 
 if __name__ == "__main__":
 	ranges, ingredients = parse_file()
-	# part_one(ranges, ingredients) # 701, solved in 301ms
+	part_one(ranges, ingredients) # 701
 	merged_ranges = merge_ranges(ranges)
-	part_two(merged_ranges, ingredients) # 
+	part_two(merged_ranges) # 352340558684863
