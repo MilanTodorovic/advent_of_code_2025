f7a0ce54e29fc49ec5aebdeac2aab0407a86abb4
Milan Todorovic <todor-90@hotmail.com>
2025-12-26 16:02:18 +0100

    Update Day_6.py
    Part two solution and refactoring. Not perfect, but it gets the job done.

--- Day_6.py
+++ Day_6.py
@@ -1,29 +1,63 @@
 def parse_file() -> list[str]:
-	# TODO needs rework to function with the second part
 	contents = []
-	with open("input.txt", 'r') as f:
+	with open("input_6.txt", 'r') as f:
 		for line in f.readlines():
-			contents.append([l.strip() for l in line.split()])
+			contents.append(line.strip('\n'))
 	return contents
 
-def part_one(contents: list[str]):
+
+def part_one(c: list[str]):
 	total = 0
-	i = 0
-	for op in contents[4]:
-		if op == "*":
-			total += int(contents[0][i]) * int(contents[1][i]) * int(contents[2][i]) * int(contents[3][i])
-		else:
-			total += int(contents[0][i]) + int(contents[1][i]) + int(contents[2][i]) + int(contents[3][i])
-		i += 1
+	len_of_col = len(c)
+	len_of_row = len(c[-1])
+	digits: list[str] = []
+	_op = ""
+	for i, op in zip(range(len_of_row), c[-1]):
+		#print(f"{i=}, {op=}")
+		if op != " ":
+			e = f"{_op}".join(d.strip() for d in digits)
+			print(f"Evaluating {e=}.")
+			total += eval(e) if e else 0
+			digits: list[str] = []
+			_op = op
+		for j in range(len_of_col-1):
+			if len(digits) == len_of_col-1:
+				digits[j] += c[j][i]
+			else:
+				digits.append(c[j][i])
+	else:
+		# TODO the code as is doesn\t trigger for the last operand and digits
+		#	this is a workaround
+		total += eval(f"{_op}".join(d.strip() for d in digits))
+
 	print("Part One:", total)
 
-def part_two(contents: list[str]):
+
+def part_two(c: list[str]):
 	total = 0
-	i = 0
-  # TODO get space between * and + -1 to get the width of the largest number
+	len_of_row = len(c[-1])
+	tmp = []
+	for i, op in zip(range(len_of_row), c[-1]):
+		if op != " ":
+			tmp.append(op)
+			n = c[0][i]+c[1][i]+c[2][i]+c[3][i]
+			tmp.append(n.strip())
+		else:
+			n = c[0][i]+c[1][i]+c[2][i]+c[3][i]
+			n = n.strip()
+			if n:
+				tmp.append(n)
+			else:
+				total += eval(f"{tmp[0]}".join(n for n in tmp[1:]))
+				tmp = []
+	else:
+		# TODO the code as is doesn\t trigger for the last operand and digits
+		#	this is a workaround
+		total += eval(f"{tmp[0]}".join(n for n in tmp[1:]))
 	print("Part Two:", total)
+
 
 if __name__ == '__main__':
 	contents = parse_file()
 	part_one(contents) # 5227286044585
-	part_two(contents)
+	part_two(contents) # 10227753257799
